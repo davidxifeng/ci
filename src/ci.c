50066c58d2215811de6962febb25a98adecd7059
David Feng <davidxifeng@gmail.com>
2016-05-10 13:27:11 +0800

    format fix: indent 4 spaces -> 2 spaces

--- src/ci.c
+++ src/ci.c
@@ -8,159 +8,159 @@
 
 
 char *p,
-     *lp, // current position in source code
-     *data;    // data/bss pointer
+   *lp, // current position in source code
+   *data;    // data/bss pointer
 
 char * bd;
 
 int *be;      // base address of text segment
 int *e, *le,  // current position in emitted code
-    *id,      // currently parsed indentifier
-    *sym,    // symbol table (simple list of identifiers)
-    ival,    // current token value
-    ty,      // current expression type
-    loc,      // local variable offset
-    line,    // current line number
-    src;      // print source and assembly flag
+  *id,      // currently parsed indentifier
+  *sym,    // symbol table (simple list of identifiers)
+  ival,    // current token value
+  ty,      // current expression type
+  loc,      // local variable offset
+  line,    // current line number
+  src;      // print source and assembly flag
 
 enum Token tk;       // current token
 
 // map toEnum [0 .. 127] :: String
 char ck_visible(char c) {
-    if (c < 33 || c > 126) {
-        return ' ';
-    } else {
-        return c;
-    }
+  if (c < 33 || c > 126) {
+    return ' ';
+  } else {
+    return c;
+  }
 }
 
 enum Action {
-    RunC = 0,
-    Compile,
-    RunBinary,
+  RunC = 0,
+  Compile,
+  RunBinary,
 };
 
 int real_main(int, char **, int, enum Action);
 
 int main(int argc, char **argv) {
-    int debug = 0;    // print executed instructions
-    enum Action action = RunC;
+  int debug = 0;    // print executed instructions
+  enum Action action = RunC;
 
+  --argc; ++argv;
+  if (argc > 0 && **argv == '-' && (*argv)[1] == 's') {
     --argc; ++argv;
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 's') {
-        --argc; ++argv;
-        src = 1;
-    }
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 'b') {
-        --argc; ++argv;
-        action = RunBinary;
-    }
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 'c') {
-        --argc; ++argv;
-        action = Compile;
-    }
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 'd') {
-        --argc; ++argv;
-        debug = 1;
-    }
-    if (argc < 1) {
-        printf("usage: ci [-s|b|c] [-d] file [...]\n");
-        printf("example:\n");
+    src = 1;
+  }
+  if (argc > 0 && **argv == '-' && (*argv)[1] == 'b') {
+    --argc; ++argv;
+    action = RunBinary;
+  }
+  if (argc > 0 && **argv == '-' && (*argv)[1] == 'c') {
+    --argc; ++argv;
+    action = Compile;
+  }
+  if (argc > 0 && **argv == '-' && (*argv)[1] == 'd') {
+    --argc; ++argv;
+    debug = 1;
+  }
+  if (argc < 1) {
+    printf("usage: ci [-s|b|c] [-d] file [...]\n");
+    printf("example:\n");
 
-        printf(ANSI_COLOR_GREEN "\tci -s test.c\n" ANSI_COLOR_RESET);
-        printf("\t\tshow compile code\n\n");
+    printf(COLOR_GREEN "\tci -s test.c\n" COLOR_RESET);
+    printf("\t\tshow compile code\n\n");
 
-        printf(ANSI_COLOR_GREEN "\tci -b [-d] test.c.bin\n\n" ANSI_COLOR_RESET);
-        printf("\t\trun bytecode\n");
+    printf(COLOR_GREEN "\tci -b [-d] test.c.bin\n\n" COLOR_RESET);
+    printf("\t\trun bytecode\n");
 
-        printf(ANSI_COLOR_GREEN "\tci [-c] test.c\n" ANSI_COLOR_RESET);
-        printf("\t\tcompile and save c code\n\n");
+    printf(COLOR_GREEN "\tci [-c] test.c\n" COLOR_RESET);
+    printf("\t\tcompile and save c code\n\n");
 
-        printf(ANSI_COLOR_GREEN "\tci [-d] test.c\n" ANSI_COLOR_RESET);
-        printf("\t\tcompile and run c code\n\n");
-        return 0;
-    }
-    return real_main(argc, argv, debug, action);
+    printf(COLOR_GREEN "\tci [-d] test.c\n" COLOR_RESET);
+    printf("\t\tcompile and run c code\n\n");
+    return 0;
+  }
+  return real_main(argc, argv, debug, action);
 }
 
 int real_main(int argc, char **argv, int debug, enum Action action) {
-    if (action == RunBinary) {
-        return run_process(argc, argv, debug);
-    }
-    int poolsz = 256*1024; // arbitrary size
+  if (action == RunBinary) {
+    return run_process(argc, argv, debug);
+  }
+  int poolsz = 256*1024; // arbitrary size
 
-    int i; // temps
+  int i; // temps
 
-    if (!(sym = malloc(poolsz))) {
-        printf("could not malloc(%d) symbol area\n", poolsz);
-        return -1;
-    }
-    if (!(be = le = e = malloc(poolsz))) {
-        printf("could not malloc(%d) text area\n", poolsz);
-        return -1;
-    }
-    if (!(bd = data = malloc(poolsz))) {
-        printf("could not malloc(%d) data area\n", poolsz);
-        return -1;
-    }
+  if (!(sym = malloc(poolsz))) {
+    printf("could not malloc(%d) symbol area\n", poolsz);
+    return -1;
+  }
+  if (!(be = le = e = malloc(poolsz))) {
+    printf("could not malloc(%d) text area\n", poolsz);
+    return -1;
+  }
+  if (!(bd = data = malloc(poolsz))) {
+    printf("could not malloc(%d) data area\n", poolsz);
+    return -1;
+  }
 
-    memset(sym, 0, poolsz);
-    memset(e,   0, poolsz);
-    memset(data, 0, poolsz);
+  memset(sym, 0, poolsz);
+  memset(e,   0, poolsz);
+  memset(data, 0, poolsz);
 
-    p = "char else enum if int return while "
-        "fopen fread fclose printf malloc memset memcmp exit";
+  p = "char else enum if int return while "
+    "fopen fread fclose printf malloc memset memcmp exit";
 
-    // add keywords to symbol table
-    for (i = Char; i <= While; i++) {
-        next();
-        id[Tk] = i;
+  // add keywords to symbol table
+  for (i = Char; i <= While; i++) {
+    next();
+    id[Tk] = i;
+  }
+
+  // add library to symbol table
+  i = OPEN;
+  while (i <= EXIT) {
+    next();
+    id[Class] = Sys;
+    id[Type] = INT;
+    id[Val] = i++;
+  }
+
+  if (!(lp = p = malloc(poolsz))) {
+    printf("could not malloc(%d) source area\n", poolsz);
+    return -1;
+  }
+
+  FILE *fd;
+  if ((fd = fopen(*argv, "r")) == 0) {
+    printf("could not fopen(%s)\n", *argv);
+    return -1;
+  }
+
+  if ((i = fread(p, 1, poolsz-1, fd)) <= 0) {
+    printf("fread() returned %d\n", i);
+    return -1;
+  }
+  p[i] = 0;
+  fclose(fd);
+  e[0] = 0xfecafeca; // magic number cafecafe (little endian)
+  // 0xfecafeca --32位补码表示的数值--> -20250934
+  // (1 << 32) - 0xfecafeca == 20250934
+
+  if ((i = parse()) == 0) {
+    if (action == Compile) {
+      struct Process * p = create_process(e, be, data, bd, sym);
+      char buf[128];
+      snprintf(buf, 128, "%s.bin", *argv);
+      save_process(buf, p);
+      free_process(p);
+      return 0;
     }
-
-    // add library to symbol table
-    i = OPEN;
-    while (i <= EXIT) {
-        next();
-        id[Class] = Sys;
-        id[Type] = INT;
-        id[Val] = i++;
-    }
-
-    if (!(lp = p = malloc(poolsz))) {
-        printf("could not malloc(%d) source area\n", poolsz);
-        return -1;
-    }
-
-    FILE *fd;
-    if ((fd = fopen(*argv, "r")) == 0) {
-        printf("could not fopen(%s)\n", *argv);
-        return -1;
-    }
-
-    if ((i = fread(p, 1, poolsz-1, fd)) <= 0) {
-        printf("fread() returned %d\n", i);
-        return -1;
-    }
-    p[i] = 0;
-    fclose(fd);
-    e[0] = 0xfecafeca; // magic number cafecafe (little endian)
-    // 0xfecafeca --32位补码表示的数值--> -20250934
-    // (1 << 32) - 0xfecafeca == 20250934
-
-    if ((i = parse()) == 0) {
-        if (action == Compile) {
-            struct Process * p = create_process(e, be, data, bd, sym);
-            char buf[128];
-            snprintf(buf, 128, "%s.bin", *argv);
-            save_process(buf, p);
-            free_process(p);
-            return 0;
-        }
-        if (src) return 0;
-        return run_c(argc, argv, debug, -1);
-    } else {
-        return i;
-    }
+    if (src) return 0;
+    return run_c(argc, argv, debug, -1);
+  } else {
+    return i;
+  }
 }
 
-// vim: tabstop=4 shiftwidth=4 softtabstop=4
+// vim: tabstop=2 shiftwidth=2 softtabstop=2
--- src/vm.c
+++ src/vm.c
@@ -10,132 +10,129 @@ extern int * be;
 extern char * bd;
 
 const char *op_codes =
-    "LEA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LGB ,"
-    "LEV ,"
-    "LI  ,LC  ,SI  ,SC  ,PSH ,"
+  "LEA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LGB ,"
+  "LEV ,"
+  "LI  ,LC  ,SI  ,SC  ,PSH ,"
 
-    "OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,"
-    "SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,"
+  "OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,"
+  "SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,"
 
-    "OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT,";
+  "OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT,";
 
 static void
 debug_info(int *pc, int i, int cycle) {
-    printf(ANSI_COLOR_GREEN "%d> %d: %.4s", cycle, (int)pc, &op_codes[i * 5]);
+  printf(COLOR_GREEN "%d> %d: %.4s", cycle, (int)pc, &op_codes[i * 5]);
 
-    // 本组指令有一个操作数
-    if (i <= LGB) {
-        printf(" %d\n" ANSI_COLOR_RESET, *pc);
-    } else {
-        printf("\n" ANSI_COLOR_RESET);
-    }
+  // 本组指令有一个操作数
+  if (i <= LGB) {
+    printf(" %d\n" COLOR_RESET, *pc);
+  } else {
+    printf("\n" COLOR_RESET);
+  }
 }
 
 int run_c(int argc, char **argv, int debug, int main_addr) {
-    int *pc, *sp, *bp = NULL, a = 0, cycle; // vm registers
+  int *pc, *sp, *bp = NULL, a = 0, cycle; // vm registers
 
-    if (main_addr == -1) {
-        int *id = sym;
-        while (id[Tk]) {
-            if (!memcmp((const void *)id[Name], "main", 4)) {
-                break;
-            }
-            id = id + Idsz;
-        }
-
-        if (!(pc = be + id[Val])) {
-            printf("main() not defined\n");
-            return -1;
-        }
-    } else {
-         pc = be + main_addr;
+  if (main_addr == -1) {
+    int *id = sym;
+    while (id[Tk]) {
+      if (!memcmp((const void *)id[Name], "main", 4)) {
+        break;
+      }
+      id = id + Idsz;
     }
 
-    int stack_size = 128 * 1024;
-    if (!(sp = malloc(stack_size * sizeof(int)))) {
-        printf("could not malloc(%d) stack area\n", stack_size);
-        return -1;
+    if (!(pc = be + id[Val])) {
+      printf("main() not defined\n");
+      return -1;
     }
+  } else {
+     pc = be + main_addr;
+  }
 
-    int i, *t; // temps
-    // setup stack
-    sp    = sp + stack_size;
-    *--sp = EXIT; // call exit if main returns
-    *--sp = PSH;
-    t     = sp;
-    *--sp = argc;
-    *--sp = (int)argv;
-    *--sp = (int)t;
+  int stack_size = 128 * 1024;
+  if (!(sp = malloc(stack_size * sizeof(int)))) {
+    printf("could not malloc(%d) stack area\n", stack_size);
+    return -1;
+  }
 
-    cycle = 0;
+  int i, *t; // temps
+  // setup stack
+  sp    = sp + stack_size;
+  *--sp = EXIT; // call exit if main returns
+  *--sp = PSH;
+  t     = sp;
+  *--sp = argc;
+  *--sp = (int)argv;
+  *--sp = (int)t;
+
+  cycle = 0;
 
 #define ci_dispatch(o)  switch(o)
 #define ci_case(c,b)    case c: {b} break;
 #define ci_default(b)   default: {b};
 
-    while (1) {
-        i = *pc++; ++cycle;
-        if (debug) debug_info(pc, i, cycle);
+  while (1) {
+    i = *pc++; ++cycle;
+    if (debug) debug_info(pc, i, cycle);
 
-        ci_dispatch(i) {
-            ci_case(LEA, a = (int)(bp + *pc++);)                     // load local address
-            ci_case(LGB, a = (int)(bd + *pc++);)                     // load global address
-            ci_case(IMM, a = *pc++;)                                 // load immediate
-            ci_case(JMP, pc = pc + *pc;)                             // jump
-            ci_case(JSR, *--sp = (int)(pc + 1); pc = be + *pc;)      // jump to subroutine
-            ci_case(BZ,  pc = a ? pc + 1 : pc + *pc;)                // branch if zero
-            ci_case(BNZ, pc = a ? pc + *pc : pc + 1;)                // branch if not zero
-            ci_case(ENT, *--sp = (int)bp; bp = sp; sp = sp - *pc++;) // enter subroutine
-            ci_case(ADJ, sp = sp + *pc++;)                           // stack adjust
+    ci_dispatch(i) {
+      ci_case(LEA, a = (int)(bp + *pc++);)                         // load local address
+      ci_case(LGB, a = (int)(bd + *pc++);)                         // load global address
+      ci_case(IMM, a = *pc++;)                                     // load immediate
+      ci_case(JMP, pc = pc + *pc;)                                 // jump
+      ci_case(JSR, *--sp = (int)(pc + 1); pc = be + *pc;)          // jump to subroutine
+      ci_case(BZ,  pc = a ? pc + 1 : pc + *pc;)                    // branch if zero
+      ci_case(BNZ, pc = a ? pc + *pc : pc + 1;)                    // branch if not zero
+      ci_case(ENT, *--sp = (int)bp; bp = sp; sp = sp - *pc++;)     // enter subroutine
+      ci_case(ADJ, sp = sp + *pc++;)                               // stack adjust
+      ci_case(LEV, sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;) // leave subroutine
 
-            ci_case(LEV, sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;) // leave subroutine
+      ci_case(LI,  a = *(int *)a;)          // load int
+      ci_case(LC,  a = *(char *)a;)         // load char
+      ci_case(SI,  *(int *)*sp++ = a;)      // store int
+      ci_case(SC,  a = *(char *)*sp++ = a;) // store char
+      ci_case(PSH, *--sp = a;)              // push
 
-            ci_case(LI,  a = *(int *)a;)          // load int
-            ci_case(LC,  a = *(char *)a;)         // load char
-            ci_case(SI,  *(int *)*sp++ = a;)      // store int
-            ci_case(SC,  a = *(char *)*sp++ = a;) // store char
-            ci_case(PSH, *--sp = a;)              // push
+      ci_case(OR,  a = *sp++ |  a;)
+      ci_case(XOR, a = *sp++ ^  a;)
+      ci_case(AND, a = *sp++ &  a;)
+      ci_case(EQ,  a = *sp++ == a;)
+      ci_case(NE,  a = *sp++ != a;)
+      ci_case(LT,  a = *sp++ <  a;)
+      ci_case(GT,  a = *sp++ >  a;)
+      ci_case(LE,  a = *sp++ <= a;)
+      ci_case(GE,  a = *sp++ >= a;)
+      ci_case(SHL, a = *sp++ << a;)
+      ci_case(SHR, a = *sp++ >> a;)
+      ci_case(ADD, a = *sp++ +  a;)
+      ci_case(SUB, a = *sp++ -  a;)
+      ci_case(MUL, a = *sp++ *  a;)
+      ci_case(DIV, a = *sp++ /  a;)
+      ci_case(MOD, a = *sp++ %  a;)
 
-            ci_case(OR,  a = *sp++ |  a;)
-            ci_case(XOR, a = *sp++ ^  a;)
-            ci_case(AND, a = *sp++ &  a;)
-            ci_case(EQ,  a = *sp++ == a;)
-            ci_case(NE,  a = *sp++ != a;)
-            ci_case(LT,  a = *sp++ <  a;)
-            ci_case(GT,  a = *sp++ >  a;)
-            ci_case(LE,  a = *sp++ <= a;)
-            ci_case(GE,  a = *sp++ >= a;)
-            ci_case(SHL, a = *sp++ << a;)
-            ci_case(SHR, a = *sp++ >> a;)
-            ci_case(ADD, a = *sp++ +  a;)
-            ci_case(SUB, a = *sp++ -  a;)
-            ci_case(MUL, a = *sp++ *  a;)
-            ci_case(DIV, a = *sp++ /  a;)
-            ci_case(MOD, a = *sp++ %  a;)
+      ci_case(OPEN, a = (int)fopen((const char *)sp[1], (const char *)*sp);)
+      ci_case(READ, a = fread((char *)sp[3], sp[2], sp[1], (FILE *)*sp);)
+      ci_case(CLOS, a = fclose((FILE *)*sp);)
+      ci_case(PRTF,
+          t = sp + pc[1];
+          a = printf((char *)t[-1], t[-2], t[-3], t[-4], t[-5], t[-6]);
+          )
+      ci_case(MALC, a = (int)malloc(*sp);)
+      ci_case(MSET, a = (int)memset((char *)sp[2], sp[1], *sp);)
+      ci_case(MCMP, a = memcmp((char *)sp[2], (char *)sp[1], *sp);)
+      ci_case(EXIT,
+          printf(COLOR_BLUE "exit(%d) cycle = %d\n" COLOR_RESET, *sp, cycle);
+          return *sp;)
 
-            ci_case(OPEN, a = (int)fopen((const char *)sp[1], (const char *)*sp);)
-            ci_case(READ, a = fread((char *)sp[3], sp[2], sp[1], (FILE *)*sp);)
-            ci_case(CLOS, a = fclose((FILE *)*sp);)
-            ci_case(PRTF,
-                    t = sp + pc[1];
-                    a = printf((char *)t[-1], t[-2], t[-3], t[-4], t[-5], t[-6]);
-                    )
-            ci_case(MALC, a = (int)malloc(*sp);)
-            ci_case(MSET, a = (int)memset((char *)sp[2], sp[1], *sp);)
-            ci_case(MCMP, a = memcmp((char *)sp[2], (char *)sp[1], *sp);)
-            ci_case(EXIT,
-                    printf(ANSI_COLOR_BLUE "exit(%d) cycle = %d\n"
-                        ANSI_COLOR_RESET, *sp, cycle);
-                    return *sp;)
-
-            ci_default(
-                    printf(ANSI_COLOR_RED "unknown instruction = %d! cycle = %d\n"
-                        ANSI_COLOR_RESET, i, cycle);
-                    return -1;)
-        }
+      ci_default(
+          printf(COLOR_RED "unknown instruction = %d! cycle = %d\n" COLOR_RESET, i, cycle);
+          return -1;)
     }
-    return 0;
+  }
+  return 0;
 }
 
 
-// vim: tabstop=4 shiftwidth=4 softtabstop=4
+// vim: tabstop=2 shiftwidth=2 softtabstop=2
