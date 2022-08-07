2e23c19f0b6b7760878ec3de58a4839e15771545
David Feng <davidxifeng@gmail.com>
2022-08-07 18:57:01 +0900

    体验了dioxus,但打算换成封装程度低一些的库

---
+++ toy/src/lex.rs
@@ -0,0 +1,10 @@
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn demo_test() {
+        assert!(true);
+    }
+}
--- toy/src/main.rs
+++ toy/src/main.rs
@@ -1,4 +1,19 @@
+use dioxus::prelude::*;
 
 fn main() {
-    println!("Hello, toy language in rust again.");
+    dioxus_tui::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            width: "100%",
+            height: "20px",
+            background_color: "black",
+            justify_content: "center",
+            align_items: "center",
+
+            "Hello world!"
+        }
+    })
 }
