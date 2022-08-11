6658410e81f5d8697671b970b7fec7d7f2ebae4f
David Feng <davidxifeng@gmail.com>
2022-08-11 14:06:09 +0900

    simple string literal lexing

--- toy/src/lex.rs
+++ toy/src/lex.rs
@@ -15,6 +15,7 @@ fn is_id_char(c: &char) -> bool {
 	is_id_initial_char(c) || is_digit(c)
 }
 
+/*
 #[derive(Debug, PartialEq)]
 enum Keyword {
 	Char,
@@ -53,22 +54,32 @@ enum Punct {
 	Brak,
 }
 
+*/
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
 	IntegerConst(String),
-	CharacterConst(char),
+	// CharacterConst(char),
 	StringLiteral(String),
-	Keyword(Keyword),
+	// Keyword(Keyword),
 	Id(String),
-	Punct(Punct),
+	// Punct(Punct),
 }
 
 #[derive(Debug, PartialEq)]
 pub enum LexError {
 	InvalidChar(char),
+	UnexpectedEofAt,
+	ExpectingCh(char),
+	UnknownEscape(char),
 }
 
-pub struct TokenApi {}
+#[derive(Debug)]
+pub struct TokenApi {
+	/// 当前行号
+	line: isize,
+	token_count: isize,
+}
 
 impl TokenApi {
 	/// 当前结构果然思路上还有严重的问题,状态不足,不能识别出词法阶段的错误
@@ -79,7 +90,7 @@ impl TokenApi {
 	/// 然后next函数只识别标识符, 并不区分 关键字 还是库函数,或者普通变量
 	/// 文档上看到说go语言解析可以不用符号表,不知道是什么意思.
 	/// 只有25个关键字, 不知是不是和词法解析有关系. 关键字和预定义标识符等内在关系上面
-	fn try_next_token(iter: &mut std::str::Chars, ts: &mut TokenState) -> Option<Result<Token, LexError>> {
+	fn try_next_token(self: &mut Self, iter: &mut std::str::Chars) -> Option<Result<Token, LexError>> {
 		// 不可以使用for in, into iter 会move走迭代器,就不能手动控制了
 		loop {
 			// 6.4 Lexical elements
@@ -98,10 +109,10 @@ impl TokenApi {
 					// 处理换行和空白
 					'\r' => {
 						iter.peeking_take_while(|&x| x == '\n').next();
-						ts.line += 1;
+						self.line += 1;
 					}
 					'\n' => {
-						ts.line += 1;
+						self.line += 1;
 					}
 					' ' | '\t' => {} // skip
 
@@ -111,21 +122,15 @@ impl TokenApi {
 						while let Some(idc) = iter.peeking_take_while(is_id_char).next() {
 							ids.push(idc);
 						}
-						ts.token_count += 1;
 						return Some(Ok(Token::Id(ids)));
 					}
 					// const处理, 应该包含int, float, char
 					_ if is_digit(&c) => {
-						let mut iv = String::from(c);
-						while let Some(nc) = iter.peeking_take_while(is_digit).next() {
-							iv.push(nc);
-						}
-						ts.token_count += 1;
-						return Some(Ok(Token::IntegerConst(iv)));
+						return self.try_decimal(iter, c);
 					}
 					// string literal
 					'"' => {
-						return Self::try_string_literal(iter);
+						return self.try_string_literal(iter);
 					}
 
 					// punctuators
@@ -136,20 +141,76 @@ impl TokenApi {
 		}
 	}
 
-	fn try_string_literal(iter: &mut std::str::Chars) -> Option<Result<Token, LexError>> {
-		// 找到匹配的 " 之前, 匹配任何内容,并放入字符串常量; 需要处理转义,和 输入提前结束的异常
+	fn try_decimal(self: &mut Self, iter: &mut std::str::Chars, c: char) -> Option<Result<Token, LexError>> {
+		let mut str = String::from(c);
+		while let Some(nc) = iter.peeking_take_while(is_digit).next() {
+			str.push(nc);
+		}
+		return Some(Ok(Token::IntegerConst(str)));
+	}
 
-		None
+	fn try_string_literal(self: &mut Self, iter: &mut std::str::Chars) -> Option<Result<Token, LexError>> {
+		// 找到匹配的 " 之前, 匹配任何内容,并放入字符串常量; 需要处理转义,和 输入提前结束的异常
+		let mut val = String::new();
+		while let Some(nc) = iter.peeking_take_while(|c| *c != '"').next() {
+			let mut v = nc;
+			if nc == '\\' {
+				if let Some(nnc) = iter.next() {
+					match nnc {
+						'\\' => (), // just skip
+						'r' => {
+							v = '\r';
+						}
+						'n' => {
+							v = '\n';
+						}
+						't' => {
+							v = '\t';
+						}
+						'"' => {
+							v = '"';
+						}
+						uc => {
+							return Some(Err(LexError::UnknownEscape(uc)));
+						}
+					}
+				} else {
+					return Some(Err(LexError::UnexpectedEofAt));
+				}
+			}
+			val.push(v);
+		}
+		// better here?
+		if let Some(err) = self.expect_next(iter, '"') {
+			return Some(Err(err));
+		}
+		return Some(Ok(Token::StringLiteral(val)));
+	}
+
+	fn expect_next(self: &mut Self, iter: &mut std::str::Chars, c: char) -> Option<LexError> {
+		if let Some(nnc) = iter.next() {
+			if nnc == c {
+				return None;
+			} else {
+				return Some(LexError::ExpectingCh(c));
+			}
+		} else {
+			return Some(LexError::UnexpectedEofAt);
+		}
 	}
 
 	/// 对输入字符串进行词法解析,得到一组token list,或者错误信息
 	pub fn parse(input: &str) -> Result<Vec<Token>, LexError> {
+		println!("now parsing: {}", input);
 		let mut token_list = vec![];
-		let mut lex_state = TokenState { line: 1, token_count: 0 };
+		let mut lex_state = TokenApi { line: 1, token_count: 0 };
 		let mut iter = input.chars();
-		while let Some(result) = Self::try_next_token(&mut iter, &mut lex_state) {
+		while let Some(result) = lex_state.try_next_token(&mut iter) {
 			match result {
-				Ok(token) => token_list.push(token),
+				Ok(token) => {
+					lex_state.token_count += 1;
+					token_list.push(token);
+				}
 				Err(err) => return Err(err),
 			}
 		}
@@ -158,14 +219,6 @@ impl TokenApi {
 
 		Ok(token_list)
 	}
-}
-
-/// 词法分析状态
-#[derive(Debug)]
-struct TokenState {
-	/// 当前行号
-	line: isize,
-	token_count: isize,
 }
 
 #[cfg(test)]
@@ -188,7 +241,7 @@ mod tests {
 	#[test]
 	fn run_lex_2() {
 		assert_eq!(
-			TokenApi::parse(r##"I am a C string"##),
+			TokenApi::parse(r##""I am a C string""##),
 			Ok(vec![Token::StringLiteral("I am a C string".to_string())])
 		);
 		// assert_eq!(TokenApi::parse("ix"), vec![Token::Id("ix".to_string())]);
