47b6bafe094524a22d2162db97833e5324124479
David Feng <davidxifeng@gmail.com>
2022-09-01 22:11:25 +0900

    print expr tree: clean up

--- rust/src/compile/impls.rs
+++ rust/src/compile/impls.rs
@@ -105,6 +105,15 @@ pub enum NodePos {
 }
 
 impl NodePos {
+	fn node_prelude(&self) -> &'static str {
+		match self {
+			NodePos::Init => "────",
+			NodePos::Top => "┌───",
+			NodePos::Middle => "├───",
+			NodePos::Bottom => "└───",
+		}
+	}
+
 	fn is_init(&self) -> bool {
 		*self == NodePos::Init
 	}
@@ -114,209 +123,99 @@ impl NodePos {
 	fn is_middle(&self) -> bool {
 		*self == NodePos::Middle
 	}
-	fn is_bottom(&self) -> bool {
-		*self == NodePos::Bottom
-	}
 }
 
-fn print_leaf(s: &mut String, p: &str, pos: &NodePos, v: impl Display) {
-	if p.is_empty() {
-		s.push_str(&style(v.to_string().as_str()).green().to_string());
-	} else {
-		let pf = if pos.is_top() {
-			"┌───"
-		} else {
-			if pos.is_middle() {
-				"├───"
-			} else if pos.is_bottom() {
-				"└───"
-			} else {
-				"────"
-			}
-		};
+fn print_op(s: &mut String, prev: &str, pos: &NodePos, v: impl Display) {
+	s.push_str(&style(prev).dim().to_string());
+	s.push_str(&style(pos.node_prelude()).dim().to_string());
+	s.push_str(&style(v.to_string().as_str()).blue().to_string());
+	s.push('\n');
+}
 
-		s.push_str(&style(p).dim().to_string());
-		s.push_str(&style(pf).dim().to_string());
-		s.push_str(&style(v.to_string().as_str()).green().to_string());
-		s.push('\n');
-	}
+fn print_leaf(s: &mut String, prev: &str, pos: &NodePos, v: impl Display) {
+	// if prev.is_empty() { s.push_str(&style(v.to_string().as_str()).green().to_string()); } else {
+	s.push_str(&style(prev).dim().to_string());
+	s.push_str(&style(pos.node_prelude()).dim().to_string());
+	s.push_str(&style(v.to_string().as_str()).green().to_string());
+	s.push('\n');
+	// }
 }
 
 fn print_binary_node(s: &mut String, prev: &str, pos: &NodePos, v: impl Display, left: &Expr, right: &Expr) {
-	let prefix_str = if pos.is_init() || pos.is_top() {
-		"    "
-	} else {
-		"│   "
-		// if pos.is_top() {
-		// 	"    "
-		// } else if pos.is_middle() {
-		// 	"│   "
-		// } else if pos.is_bottom() {
-		// 	"│   "
-		// } else {
-		// 	"│   "
-		// }
-	};
-	print_expr_tree(left, s, &(prev.to_owned() + prefix_str), &NodePos::Top);
+	let next_prefix_top = if pos.is_init() || pos.is_top() { "    " } else { "│   " };
+	print_expr_tree(left, s, &(prev.to_owned() + next_prefix_top), &NodePos::Top);
 
-	let op_prefix = if prev.is_empty() {
-		"────"
-	} else if pos.is_top() {
-		"┌───"
-	} else if pos.is_middle() {
-		"├───"
-	} else {
-		"└───"
-	};
+	print_op(s, prev, pos, v);
 
-	s.push_str(&style(prev).dim().to_string());
-	s.push_str(&style(op_prefix).dim().to_string());
-	s.push_str(&style(v.to_string().as_str()).bold().blue().to_string());
-	s.push('\n');
-
-	print_expr_tree(
-		right,
-		s,
-		&(prev.to_owned() + if pos.is_top() || pos.is_middle() { "│   " } else { "    " }),
-		&NodePos::Bottom,
-	);
+	let next_prefix = if pos.is_top() || pos.is_middle() { "│   " } else { "    " };
+	print_expr_tree(right, s, &(prev.to_owned() + next_prefix), &NodePos::Bottom);
 }
 
-fn print_multi_node(s: &mut String, prev: &str, top: &NodePos, v: impl Display, expr: &Vec<Expr>) {
-	if !expr.is_empty() {
-		let prefix_str = if top.is_top() || prev.is_empty() { "    " } else { "│   " };
+fn print_multi_node(s: &mut String, prev: &str, pos: &NodePos, v: impl Display, expr: &Vec<Expr>) {
+	if expr.is_empty() {
+		print_op(s, prev, pos, v);
+	} else {
+		let prefix_str = if pos.is_top() || prev.is_empty() { "    " } else { "│   " };
 		print_expr_tree(&expr[0], s, &(prev.to_owned() + prefix_str), &NodePos::Top);
 
-		let op_prefix = if prev.is_empty() {
-			"────"
-		} else if top.is_top() {
-			"┌───"
-		} else if top.is_middle() {
-			"├───"
-		} else {
-			"└───"
-		};
-		s.push_str(&style(prev).dim().to_string());
-		s.push_str(&style(op_prefix).dim().to_string());
-		s.push_str(&style(v.to_string().as_str()).bold().blue().to_string());
-		s.push('\n');
-	}
+		print_op(s, prev, pos, v);
 
-	let mut iter = expr.iter().skip(1).peekable();
-	while let Some(e) = iter.next() {
-		let is_last = iter.peek().is_none();
-		print_expr_tree(
-			e,
-			s,
-			&(prev.to_owned() + if top.is_top() || top.is_middle() { "│   " } else { "    " }),
-			if is_last { &NodePos::Bottom } else { &NodePos::Middle },
-		);
+		let mut iter = expr.iter().skip(1).peekable();
+		let next_prefix = prev.to_owned() + if pos.is_top() || pos.is_middle() { "│   " } else { "    " };
+		while let Some(e) = iter.next() {
+			print_expr_tree(
+				e,
+				s,
+				&next_prefix,
+				if iter.peek().is_none() { &NodePos::Bottom } else { &NodePos::Middle },
+			);
+		}
 	}
 }
 
 fn print_expr_tree(this: &Expr, s: &mut String, prev: &str, pos: &NodePos) {
 	match this {
-		Expr::Const(v) => {
-			print_leaf(s, prev, &pos, v);
-		}
-		Expr::StringLiteral(v) => {
-			print_leaf(s, prev, &pos, v);
-		}
-		Expr::Id(v) => {
-			print_leaf(s, prev, &pos, v);
-		}
+		Expr::Const(v) => print_leaf(s, prev, pos, v),
 
-		Expr::BinOp(BinOp { op, left, right }) => {
-			print_binary_node(s, prev, pos, op, left, right);
-		}
-		Expr::AssignExpr(AssignExpr { left, assign, right }) => {
-			print_binary_node(s, prev, pos, assign, left, right);
-		}
+		Expr::StringLiteral(v) => print_leaf(s, prev, pos, v),
+
+		Expr::Id(v) => print_leaf(s, prev, pos, v),
+
+		Expr::BinOp(BinOp { op, left, right }) => print_binary_node(s, prev, pos, op, left, right),
+
+		Expr::AssignExpr(AssignExpr { left, assign, right }) => print_binary_node(s, prev, pos, assign, left, right),
+
 		Expr::CommaExpr(CommaExpr { expr }) => print_multi_node(s, prev, pos, ",", expr),
+
 		Expr::CondExpr(CondExpr { cond, left, right }) => {
 			let prefix_str = if pos.is_top() || prev.is_empty() { "    " } else { "│   " };
 			print_expr_tree(&cond, s, &(prev.to_owned() + prefix_str), &NodePos::Top);
 
-			let op_prefix = if prev.is_empty() {
-				"────"
-			} else if pos.is_top() {
-				"┌───"
-			} else if pos.is_middle() {
-				"├───"
-			} else {
-				"└───"
-			};
+			print_op(s, prev, pos, "? :");
 
-			s.push_str(&style(prev).dim().to_string());
-			s.push_str(&style(op_prefix).dim().to_string());
-			s.push_str(&style("? :").bold().blue().to_string());
-			s.push('\n');
-
-			print_expr_tree(
-				&left,
-				s,
-				&(prev.to_owned() + if pos.is_top() || pos.is_middle() { "│   " } else { "    " }),
-				&NodePos::Middle,
-			);
-			print_expr_tree(
-				&right,
-				s,
-				&(prev.to_owned() + if pos.is_top() || pos.is_middle() { "│   " } else { "    " }),
-				&NodePos::Bottom,
-			);
+			let next_prefix = prev.to_owned() + if pos.is_top() { "│   " } else { "    " };
+			print_expr_tree(&left, s, &next_prefix, &NodePos::Middle);
+			print_expr_tree(&right, s, &next_prefix, &NodePos::Bottom);
 		}
 		Expr::SimplePostfix(PostfixOP { op, expr }) => {
-			let op_prefix = if prev.is_empty() {
-				"────"
-			} else if pos.is_top() {
-				"┌───"
-			} else if pos.is_middle() {
-				"├───"
-			} else {
-				"└───"
-			};
+			print_op(s, prev, pos, op);
 
-			s.push_str(&style(prev).dim().to_string());
-			s.push_str(&style(op_prefix).dim().to_string());
-			s.push_str(&style(op.to_string().as_str()).bold().blue().to_string());
-			s.push('\n');
-
-			print_expr_tree(expr, s, &(prev.to_owned() + if pos.is_top() { "│   " } else { "    " }), &NodePos::Middle);
+			let next_prefix = prev.to_owned() + if pos.is_top() { "│   " } else { "    " };
+			print_expr_tree(expr, s, &next_prefix, &NodePos::Middle);
 		}
 		Expr::UnaryOp(UnaryOp { op, expr }) => {
-			let op_prefix = if pos.is_init() {
-				"────"
-			} else if pos.is_top() {
-				"┌───"
-			} else if pos.is_middle() {
-				"├───"
-			} else {
-				"└───"
-			};
+			print_op(s, prev, pos, op);
 
-			s.push_str(&style(prev).dim().to_string());
-			s.push_str(&style(op_prefix).dim().to_string());
-			s.push_str(&style(op.to_string().as_str()).bold().blue().to_string());
-			s.push('\n');
-
-			print_expr_tree(
-				&expr,
-				s,
-				&(prev.to_owned() + if pos.is_top() { "│   " } else { "    " }),
-				&NodePos::Bottom,
-			);
+			let next_prefix = prev.to_owned() + if pos.is_top() { "│   " } else { "    " };
+			print_expr_tree(&expr, s, &next_prefix, &NodePos::Bottom);
 		}
 	}
 }
 
 impl Display for Expr {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		if f.alternate() {
-			f.write_str("expr")
-		} else {
-			let mut s = String::new();
-			print_expr_tree(self, &mut s, "", &NodePos::Init);
-			f.write_str(s.as_str())
-		}
+		let mut s = String::new();
+		print_expr_tree(self, &mut s, "", &NodePos::Init);
+		f.write_str(s.as_str())
 	}
 }
